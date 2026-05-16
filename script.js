bb3bedd57db88c15a29cff1fc0e3cc6bb23b9e39
BRAG4 <tacianobraghiroli@gmail.com>
2026-05-15 22:57:14 -0300

    ML 1.1

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-// GLOW
-document.addEventListener("mousemove", e => {
-  const glow = document.querySelector(".cursor-glow");
-  glow.style.left = e.clientX + "px";
-  glow.style.top = e.clientY + "px";
-});
+const nomes = ["Lucas", "Ana", "Pedro", "Julia"];
 
-// POPUP FAKE
-const nomes = ["Carlos", "Ana", "Lucas", "Marcos", "Julia"];
-
-function mostrarPopup() {
-  const popup = document.getElementById("popup");
+function popup() {
+  const el = document.getElementById("popup");
   const nome = nomes[Math.floor(Math.random()*nomes.length)];
 
-  popup.innerText = nome + " acabou de comprar!";
-  popup.style.display = "block";
+  el.innerText = nome + " acabou de comprar!";
+  el.style.display = "block";
 
-  setTimeout(() => popup.style.display = "none", 3000);
+  setTimeout(() => el.style.display = "none", 3000);
 }
 
-setInterval(mostrarPopup, 5000);
+setInterval(popup, 6000);
