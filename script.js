d9d2cd4dd4916cdad4333e29aab41271f7e38157
BRAG4 <tacianobraghiroli@gmail.com>
2026-05-15 22:46:41 -0300

    ML prof

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const cards = document.querySelectorAll('.card');
-
-window.addEventListener('scroll', () => {
-  cards.forEach(card => {
-    const top = card.getBoundingClientRect().top;
-
-    if (top < window.innerHeight - 50) {
-      card.classList.add('show');
-    }
-  });
+// GLOW
+document.addEventListener("mousemove", e => {
+  const glow = document.querySelector(".cursor-glow");
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
 });
+
+// POPUP FAKE
+const nomes = ["Carlos", "Ana", "Lucas", "Marcos", "Julia"];
+
+function mostrarPopup() {
+  const popup = document.getElementById("popup");
+  const nome = nomes[Math.floor(Math.random()*nomes.length)];
+
+  popup.innerText = nome + " acabou de comprar!";
+  popup.style.display = "block";
+
+  setTimeout(() => popup.style.display = "none", 3000);
+}
+
+setInterval(mostrarPopup, 5000);
